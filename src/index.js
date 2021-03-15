9e1b60dee41a2cc66a4b8b643b24ad3326d4b61b
killcodings <kontaktid@ya.ru>
2021-03-16 00:48:48 +0300

    docs: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 === 0) {
+    let strTemp = "";
+    while (str.length > 2) {
+      strTemp = str;
+      for (let i = 0; i < bracketsConfig.length; i++) {
+        str = str.replace(bracketsConfig[i].join(""), "");
+      }
+      if (strTemp == str) {
+        break;
+      }
+    }
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i].join(""), "");
+    }
+  }
+  return str === "" ? true : false;
+};
